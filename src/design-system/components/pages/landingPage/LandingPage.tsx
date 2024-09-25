5885b602cfe8222234caeaae7d30401d2a8a3e9e
sharvin <sharvin.pingulkar@akqa.com>
2024-09-26 04:10:48 +1000

    feat(app: React-App): updates pages to use design-system components and adds navigate hook

--- src/design-system/components/pages/landingPage/LandingPage.tsx
+++ src/design-system/components/pages/landingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ import { LandingPageProps } from './LandingPage.types'
 
 export const LandingPage = ({ isPrimaryBanner, primaryBanner, secondaryBanner, children }: LandingPageProps) => {
   return (
-    <BaseLayout dataTestid="landing-page" banner={isPrimaryBanner ? primaryBanner : secondaryBanner}>
+    <BaseLayout dataTestid="landing-page" isStickyNavigation banner={isPrimaryBanner ? primaryBanner : secondaryBanner}>
       {children}
     </BaseLayout>
   )
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+
+import { Button } from '../design-system/components'
+import { LandingPage as LandingPageComponent } from '../design-system/components/pages/landingPage/LandingPage'
+import { Routes as Paths } from '../route/Routes'
 export const LandingPage = () => {
+  const navigate = useNavigate()
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <LandingPageComponent isPrimaryBanner primaryBanner={<p>This is the primary banner</p>}>
+      <div>
+        <Button onClick={() => navigate(Paths.Home)} primary size="large" label="Home" />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </p>
+    </LandingPageComponent>
   )
 }
