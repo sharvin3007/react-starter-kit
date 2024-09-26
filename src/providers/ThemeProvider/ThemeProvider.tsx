6db01b13939a25c7a2b3f0234af68825b9d63879
sharvin <sharvin.pingulkar@akqa.com>
2024-09-26 15:33:23 +1000

    chore: adds theme provider and uses useTheme hook by wrapping components

---
+++ src/providers/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from 'react'
+
+import { Theme, ThemeContextProps } from './ThemeProvider.types'
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined)
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = () => {
+  const context = useContext(ThemeContext)
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider')
+  }
+  return context
+}
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>('light')
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>
+}
